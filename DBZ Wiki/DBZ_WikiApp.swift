aadc9bf8985ca526b94c123124388a7649244d04
Luis Galvan <luiiyigandark247@gmail.com>
2025-03-23 07:14:28 -0600

    Initial Commit

---
+++ DBZ Wiki/DBZ_WikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBZ_WikiApp.swift
+//  DBZ Wiki
+//
+//  Created by Luis Galvan on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DBZ_WikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
